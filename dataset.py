ebf1946a9f5672a12239e755ea4eafce1a030edf
Luckky <guodashun@163.com>
2019-11-26 16:51:58 +0800

    first test for dataset

--- dataset.py
+++ dataset.py
@@ -1,13 +1,46 @@
 import xml.etree.ElementTree as ET
 import os
+from PIL import Image
+import torch
 
-tag_path = "./data/11.21/marked/robot"
-tags = os.listdir(tag_path)
 
-for tag_dir in tags:
-    tag = ET.parse(tag_path + "/" + tag_dir)
-    cnt = 0
-    for obj in tag.iterfind('object'):
-        if obj.findtext('name') == "robot":
-            cnt = cnt + 1
-            print("The %dth robot is " % (cnt), obj.findtext("bndbox/xmin"))
+class MultiRobotsDataset(object):
+    def __init__(self, root, transforms):
+        self.root = root
+        self.transforms = transforms
+        self.imgs = list(sorted(os.listdir(os.path.join(self.root, "imgs"))))
+        self.boxes = list(sorted(os.listdir(os.path.join(self.root, "annotation_robot"))))
+
+    def __getitem__(self, idx):
+        img_path = os.path.join(self.root, "imgs", self.imgs[idx])
+        box_path = os.path.join(self.root, "annotation_robot", self.boxes[idx])
+        img = Image.open(img_path).convert("RGB")
+        image_id = torch.tensor([idx])
+        boxes = []
+        anno = ET.parse(box_path)
+        for obj in anno.iterfind('object'):
+            if obj.findtext('name') == "robot":
+                xmin = obj.findtext("bndbox/xmin")
+                xmax = obj.findtext("bndbox/xmax")
+                ymin = obj.findtext("bndbox/ymin")
+                ymax = obj.findtext("bndbox/ymax")
+                boxes.append([xmin, xmax, ymin, ymax])
+
+        target = {}
+        target["image_id"] = image_id
+        target["boxes"] = boxes
+
+        if self.transforms is not None:
+            img, target = self.transforms(img, target)
+
+        return img, target
+
+    def __len__(self):
+        return len(self.imgs)
+
+
+# test
+if __name__ == '__main__':
+    module = MultiRobotsDataset("./data/11.21/marked/", None)
+    _, target = module.__getitem__(3)
+    print(target["boxes"][0][0])
